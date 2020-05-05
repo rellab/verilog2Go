24dce616e3e84d1a92d43f530e70f58240fd2aa8
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-05-05 10:53:29 +0900

    read file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 
 	parser "github.com/verilog2Go/generated/verilog"
 	verilog "github.com/verilog2Go/src/verilog2Go"
@@ -10,16 +11,22 @@ import (
 )
 
 func main() {
-	input := antlr.NewInputStream("a+b*c+ d[2] + e")
-
+	//ファイル読み取り
+	bytes, err := ioutil.ReadFile("./src/examples/adder.v")
+	if err != nil {
+		panic(err)
+	}
+	input := antlr.NewInputStream(string(bytes))
+	//lexerの作成
 	lexer := parser.NewVerilogLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, 0)
+	//parserの作成
 	p := parser.NewVerilogParser(stream)
 
 	// リスナーをセットする
 	listener := verilog.NewVerilogListener()
 	p.AddParseListener(listener)
 
-	tree := p.Expression()
+	tree := p.Source_text()
 	fmt.Println(tree.ToStringTree([]string{}, p))
 }
--- src/verilog2Go/verilog_custom_listener.go
+++ src/verilog2Go/verilog_custom_listener.go
@@ -16,7 +16,22 @@ func NewVerilogListener() *CustomVerilogListener {
 	return &CustomVerilogListener{}
 }
 
+// EnterModule_declaration is called when production module_declaration is entered.
+func (s *CustomVerilogListener) EnterModule_declaration(ctx *parser.Module_declarationContext) {
+	//モジュールの初期化
+}
+
+// ExitModule_declaration is called when production module_declaration is exited.
+func (s *CustomVerilogListener) ExitModule_declaration(ctx *parser.Module_declarationContext) {
+	fmt.Println(ctx.Module_identifier().GetText())
+}
+
 // ExitInput_declaration is called when production input_declaration is exited.
 func (s *CustomVerilogListener) ExitInput_declaration(ctx *parser.Input_declarationContext) {
-	fmt.Println(ctx)
+	fmt.Println(ctx.GetText())
+}
+
+// ExitOutput_declaration is called when production output_declaration is exited.
+func (s *CustomVerilogListener) ExitOutput_declaration(ctx *parser.Output_declarationContext) {
+	fmt.Println(ctx.GetText())
 }
