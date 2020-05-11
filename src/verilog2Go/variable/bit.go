99afd7e4f279a675916520767518f13519fcc8a9
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-05-11 13:04:51 +0900

    ビット配列の演算を追加

--- src/verilog2Go/variable/bit.go
+++ src/verilog2Go/variable/bit.go
@@ -33,14 +33,70 @@ func (b *Bit) SetValue(value bool) {
 
 //Set はBitArrayのBitsに値をセットする
 func (ba *BitArray) Set(value int) {
-	var length = len(ba.bits)
-	var comparison = 1 << length
+	length := len(ba.bits)
+	comparison := 1 << length
 	for i := 1; i <= length; i++ {
-		// if (((value << i) & comparison) >> length) == 1 {
-		// 	ba.bits[length - i].SetValue(true)
-		// } else{
-		// 	ba.bits[length - i].SetValue(false)
-		// }
 		ba.bits[length-i].SetValue((((value << i) & comparison) >> length) == 1)
+	}
+}
+
+//ToInt はBitArrayの持つ値を10進数で返す
+func (ba BitArray) ToInt() int {
+	ret := 0
+	length := len(ba.bits)
+	for i := length - 1; i >= 0; i-- {
+		//bitの値がtrueの場合，対応する値を加算する
+		if ba.bits[i].value {
+			ret |= 1 << i
+		}
+	}
+	return ret
+}
+
+// Calc はvalueの値をもつBitArrayを返す
+func (ba *BitArray) Calc(value int, length int) BitArray {
+	comparison := 1 << length
+	var result BitArray
+	result.InitializeBitArray(length)
+	for i := 1; i <= length; i++ {
+		result.bits[length-i].SetValue((((value << i) & comparison) >> length) == 1)
+	}
+	return result
+}
+
+//Add はポート同士の加算を行う
+func (ba *BitArray) Add(input BitArray) BitArray {
+	a := ba.ToInt()
+	b := input.ToInt()
+	length := len(ba.bits)
+	var result BitArray
+	result = result.Calc(a+b, length)
+	return result
+}
+
+//Sub はポート同士の減算を行う
+func (ba *BitArray) Sub(input BitArray) BitArray {
+	a := ba.ToInt()
+	b := input.ToInt()
+	length := len(ba.bits)
+	var result BitArray
+	result = result.Calc(a-b, length)
+	return result
+}
+
+//Mul はポート同士の乗算を行う
+func (ba *BitArray) Mul(input BitArray) BitArray {
+	a := ba.ToInt()
+	b := input.ToInt()
+	length := len(ba.bits)
+	var result BitArray
+	result = result.Calc(a*b, length)
+	return result
+}
+
+func (ba *BitArray) Assign(result BitArray) {
+	length := len(ba.bits)
+	for i := 0; i < length; i++ {
+		ba.bits[i].value = result.bits[i].value
 	}
 }
--- src/verilog2Go/variable/bit_test.go
+++ src/verilog2Go/variable/bit_test.go
@@ -1,7 +1,6 @@
 package variable
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,31 +9,85 @@ import (
 func TestInitialize(t *testing.T) {
 	var bit Bit
 	bit.InitializeBit(1)
-	assert.Equal(t, bit.value, false)
-	assert.Equal(t, bit.index, 1)
+	assert.Equal(t, false, bit.value)
+	assert.Equal(t, 1, bit.index)
 }
 
 func TestSetValue(t *testing.T) {
 	var bit Bit
 	bit.InitializeBit(0)
-	assert.Equal(t, bit.value, false)
-	assert.Equal(t, bit.index, 0)
+	assert.Equal(t, false, bit.value)
+	assert.Equal(t, 0, bit.index)
 	bit.SetValue(true)
-	assert.Equal(t, bit.value, true)
-	assert.Equal(t, bit.index, 0)
+	assert.Equal(t, true, bit.value)
+	assert.Equal(t, 0, bit.index)
 }
 
 func TestInitializeBitArray(t *testing.T) {
 	var ba BitArray
 	ba.InitializeBitArray(3)
-	fmt.Println(ba.bits)
+	assert.Equal(t, false, ba.bits[0].value)
+	assert.Equal(t, false, ba.bits[1].value)
+	assert.Equal(t, false, ba.bits[2].value)
 }
 
 func TestSet(t *testing.T) {
 	var ba BitArray
 	ba.InitializeBitArray(3)
 	ba.Set(3)
-	assert.Equal(t, ba.bits[0].value, true)
-	assert.Equal(t, ba.bits[1].value, true)
-	assert.Equal(t, ba.bits[2].value, false)
+	assert.Equal(t, true, ba.bits[0].value)
+	assert.Equal(t, true, ba.bits[1].value)
+	assert.Equal(t, false, ba.bits[2].value)
+}
+
+func TestToInt(t *testing.T) {
+	var ba BitArray
+	ba.InitializeBitArray(3)
+	ba.Set(5)
+	assert.Equal(t, 5, ba.ToInt())
+	//オーバーフロー
+	ba.Set(10)
+	assert.Equal(t, 2, ba.ToInt())
+}
+
+func TestAdd(t *testing.T) {
+	var a BitArray
+	a.InitializeBitArray(3)
+	a.Set(2)
+	var b BitArray
+	b.InitializeBitArray(3)
+	b.Set(3)
+	assert.Equal(t, 5, a.Add(b).ToInt())
+}
+
+func TestASub(t *testing.T) {
+	var a BitArray
+	a.InitializeBitArray(3)
+	a.Set(3)
+	var b BitArray
+	b.InitializeBitArray(3)
+	b.Set(2)
+	assert.Equal(t, 1, a.Sub(b).ToInt())
+	//オーバーフロー
+	assert.Equal(t, 7, b.Sub(a).ToInt())
+}
+
+func TestMul(t *testing.T) {
+	var a BitArray
+	a.InitializeBitArray(3)
+	a.Set(2)
+	var b BitArray
+	b.InitializeBitArray(3)
+	b.Set(2)
+	assert.Equal(t, 4, a.Mul(b).ToInt())
+}
+
+func TestAssign(t *testing.T) {
+	var a BitArray
+	a.InitializeBitArray(3)
+	a.Set(2)
+	var b BitArray
+	b.InitializeBitArray(3)
+	b.Assign(a)
+	assert.Equal(t, 2, b.ToInt())
 }
