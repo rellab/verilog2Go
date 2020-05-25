df1fcb3436efecdf2414dce0e68a7748953fb42c
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-05-25 12:51:44 +0900

    ポートの演算を追加

--- src/builder/variable/port.go
+++ src/builder/variable/port.go
@@ -4,3 +4,35 @@ package variable
 type Port struct {
 	BitArray
 }
+
+//ToInt はBitArrayの持つ値を10進数で返す
+func (port Port) ToInt() int {
+	ret := 0
+	length := len(port.bits)
+	for i := length - 1; i >= 0; i-- {
+		//bitの値がtrueの場合，対応する値を加算する
+		if port.bits[i].value {
+			ret |= 1 << i
+		}
+	}
+	return ret
+}
+
+func (port *Port) Add(input Port) Port {
+	result := Port{port.BitArray.Add(input.BitArray)}
+	return result
+}
+
+func (port *Port) Sub(input Port) Port {
+	result := Port{port.BitArray.Sub(input.BitArray)}
+	return result
+}
+
+func (port *Port) Mul(input Port) Port {
+	result := Port{port.BitArray.Mul(input.BitArray)}
+	return result
+}
+
+func (port *Port) Assign(input Port) {
+	port.BitArray.Assign(input.BitArray)
+}
--- src/builder/variable/port_test.go
+++ src/builder/variable/port_test.go
@@ -13,3 +13,13 @@ func TestInitPort(t *testing.T) {
 	assert.Equal(t, false, port.bits[1].value)
 	assert.Equal(t, false, port.bits[2].value)
 }
+
+func TestAddPort(t *testing.T) {
+	var a Port
+	a.InitBitArray(3)
+	a.Set(2)
+	var b Port
+	b.InitBitArray(3)
+	b.Set(3)
+	assert.Equal(t, 5, a.Add(b).ToInt())
+}
