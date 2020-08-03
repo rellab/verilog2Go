9ae1ab02188197d6ba5ecbbdb54be2a49a14d2cd
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2020-08-03 12:53:24 +0900

    if文のリスナーを追加

--- src/builder/always_listener.go
+++ src/builder/always_listener.go
@@ -1,6 +1,7 @@
 package builder
 
 import (
+	"fmt"
 	"strings"
 
 	parser "github.com/verilog2Go/antlr/verilog"
@@ -33,4 +34,24 @@ func (s *CustomVerilogListener) ExitEvent_expression(ctx *parser.Event_expressio
 			AddNegedgeObserver(id)
 		}
 	}
+}
+
+// EnterConditional_statement is called when production conditional_statement is exited.
+func (s *CustomVerilogListener) EnterConditional_statement(ctx *parser.Conditional_statementContext) {
+	//if文開始
+}
+
+// ExitConditional_statement is called when production conditional_statement is exited.
+func (s *CustomVerilogListener) ExitConditional_statement(ctx *parser.Conditional_statementContext) {
+	fmt.Println(ctx.Expression().GetText())
+}
+
+// ExitStatement_or_null is called when production statement_or_null is exited.
+func (s *CustomVerilogListener) ExitStatement_or_null(ctx *parser.Statement_or_nullContext) {
+	fmt.Println("else " + ctx.GetText())
+}
+
+// ExitNonblocking_assignment is called when production nonblocking_assignment is exited.
+func (s *CustomVerilogListener) ExitNonblocking_assignment(ctx *parser.Nonblocking_assignmentContext) {
+	fmt.Println("nonblocking " + ctx.GetText())
 }
