3664f231e723513eb5583585a7ac2b4e784cad7f
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2021-02-28 18:46:40 +0900

    複数インスタンスでのスレッディングを削除

--- src/variable/observer.go
+++ src/variable/observer.go
@@ -1,10 +1,5 @@
 package variable
 
-import (
-	"fmt"
-	"sync"
-)
-
 type PosedgeObserver interface {
 	PreAlways() []BitArray
 	Always([]BitArray)
@@ -25,35 +20,26 @@ func (ba *BitArray) AddNegedgeObserver(no NegedgeObserver) {
 
 func (ba BitArray) NotifyPosedgeObserver() {
 	// 全てのPreAlways終了後にAlwaysを実行する
-	var wg sync.WaitGroup
 	var bitArrays = make([][]BitArray, len(ba.pos))
 	for i := 0; i < len(ba.pos); i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			// fmt.Println("start")
-			bitArrays[i] = ba.pos[i].PreAlways()
-			// fmt.Printf("%d, %d\n", i, bitArrays[i][1].ToInt())
-		}(i)
+		bitArrays[i] = ba.pos[i].PreAlways()
 	}
-	// 上記処理の終了を待つ
-	wg.Wait()
-	fmt.Println("end")
+	// fmt.Println("end")
 	// Alwaysを実行する
 	for i := len(ba.pos) - 1; i >= 0; i-- {
-		// ba.pos[i].Always(bitArrays[i])
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			ba.pos[i].Always(bitArrays[i])
-		}(i)
+		ba.pos[i].Always(bitArrays[i])
 	}
-	wg.Wait()
 }
 
 func (ba BitArray) NotifyNegedgeObserver() {
-	// ba.neg.Always()
-	// for i := 0; i < len(ba.neg); i++ {
-	// 	ba.neg[i].Always()
-	// }
+	// 全てのPreAlways終了後にAlwaysを実行する
+	var bitArrays = make([][]BitArray, len(ba.neg))
+	for i := 0; i < len(ba.neg); i++ {
+		bitArrays[i] = ba.neg[i].PreAlways()
+	}
+	// fmt.Println("end")
+	// Alwaysを実行する
+	for i := len(ba.neg) - 1; i >= 0; i-- {
+		ba.neg[i].Always(bitArrays[i])
+	}
 }
