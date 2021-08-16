abd3cdf2f08c042c11a741b24152ec8924870615
Yamakatsu63 <yamakatsu1217.63@gmail.com>
2021-08-17 05:18:36 +0900

    組み合わせ回路のrunの作成

--- generated/adder.go
+++ generated/adder.go
@@ -2,20 +2,46 @@ package generated
 
 import "github.com/verilog2Go/src/variable"
 
-type Adder struct {
-	a, b, q *variable.BitArray
+type Adder struct{
+    a, b, q *variable.BitArray
 }
 
-func NewAdder(args *Adder) Adder {
-	return *args
+func NewAdder(args *Adder) Adder{
+    return *args
 }
 
-func NewGoroutineAdder(in []chan variable.BitArray, out []chan variable.BitArray) *Adder {
-	Adder := &Adder{variable.NewBitArray(3), variable.NewBitArray(3), variable.NewBitArray(3)}
-	Adder.run(in, out)
-	return Adder
+func NewGoroutineAdder (in []chan int, out []chan int) *Adder{
+    Adder := &Adder{variable.NewBitArray(4), variable.NewBitArray(4), variable.NewBitArray(4)}
+    go Adder.run(in, out)
+    return Adder
 }
 
-func (Adder *Adder) Exec() {
-	Adder.q.Assign(Adder.b.Add(*Adder.a))
+func (adder *Adder) Exec() {
+    adder.q.Assign(adder.b.Add(*adder.a))
 }
+
+func (adder *Adder) run(in []chan int, out []chan int) {
+    defer close(out[0])
+    for {
+        select {
+        case v, ok := <-in[0]:
+            if ok {
+                adder.a.Set(v)
+                adder.Exec()
+                out[0] <- adder.q.ToInt()
+            } else {
+                return 
+            }
+        case v, ok := <-in[1]:
+            if ok {
+                adder.b.Set(v)
+                adder.Exec()
+                out[0] <- adder.q.ToInt()
+            } else {
+                return 
+            }
+        }
+    }
+}
+
+
--- generated/adder_test.go
+++ generated/adder_test.go
@@ -1,6 +1,7 @@
 package generated
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,7 +13,7 @@ func TestAdder(t *testing.T) {
 	a.InitBitArray(3)
 	b.InitBitArray(3)
 	q.InitBitArray(3)
-	adder := Adder(&adder{&a, &b, &q})
+	adder := NewAdder(&Adder{&a, &b, &q})
 	a.Set(2)
 	b.Set(3)
 	adder.Exec()
@@ -25,4 +26,34 @@ func TestAdder(t *testing.T) {
 	b.Set(5)
 	adder.Exec()
 	assert.Equal(t, 2, q.ToInt())
+}
+
+func TestAdderGoroutine(t *testing.T) {
+	a := make(chan int)
+	b := make(chan int)
+	q := make(chan int)
+
+	NewGoroutineAdder([]chan int{a, b}, []chan int{q})
+
+	go func() {
+		defer close(a)
+		defer close(b)
+		a <- 1
+		b <- 2
+		b <- 4
+		a <- 7
+		b <- 11
+	}()
+
+	for {
+		select {
+		case q, ok := <-q:
+			if ok {
+				fmt.Println(q)
+			} else {
+				// アウトプットチャンネルがクローズされたら終了
+				return
+			}
+		}
+	}
 }
